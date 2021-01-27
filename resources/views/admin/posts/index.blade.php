b4305097ddebb2f9215d56bfdba9ec51fcda7aac
Marco Guidi <mark.88g@gmail.com>
2021-01-27 16:54:13 +0100

    + aggiunta form delete in index Posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -43,9 +43,15 @@
                                     <a href="{{ route('admin.posts.edit', ['post' => $post -> id]) }}" class="btn btn-outline-dark">
                                         Modifica
                                     </a>
-                                    <a href="#" class="btn btn-outline-danger">
+                                    <form class="d-inline-block" action="{{ route('admin.posts.destroy', ['post' => $post -> id]) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        
+                                        <button type="submit" name="button" class="btn btn-outline-danger">Elimina</button>
+                                    </form>
+                                    {{-- <a href="#" class="btn btn-outline-danger">
                                         Elimina
-                                    </a>
+                                    </a> --}}
                                 </td>
                             </tr>
                         @endforeach
