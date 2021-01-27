e3b4805f587e95bbc80924c8ffe98fa6999c94e5
Marco Guidi <mark.88g@gmail.com>
2021-01-27 21:19:29 +0100

    + dashboard.index recuperato utente

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -14,6 +16,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.home');
+        $data = [
+            'this_user' => Auth::user()
+        ];
+        return view('admin.home', $data);
     }
 }
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,6 +7,7 @@
         <div class="row">
             <div class="col-12">
                 <h1>Dashboard</h1>
+                <p>Ciao {{ $this_user -> name }}!</p>
             </div>
         </div>
     </div>
