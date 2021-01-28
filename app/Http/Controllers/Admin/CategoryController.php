2ddd26e460e0153153f5754a7b2d7a0965728c8b
Marco Guidi <mark.88g@gmail.com>
2021-01-28 14:12:16 +0100

    + aggiunta categorie

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Category;
 
 class CategoryController extends Controller
@@ -39,7 +40,31 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $category_input = $request -> all();
+        // dd($category_input);
+
+        $new_category = new Category();
+        $new_category -> fill($category_input);
+
+        //generate slug and check
+        // 1) get slug base
+        $base_slug = Str::slug($new_category -> name, '-');
+        $slug = $base_slug;
+        // 2) sentinel var
+        $already_slug = Category::where('slug', $slug) -> first(); // return collection if there is, NULL otherwise
+        $contatore = 1; // contatore
+        // 3) check while there's not same-slug
+        while ($already_slug) {
+            $slug = $base_slug . '-' . $contatore;
+            $contatore++;
+            $already_slug = Category::where('slug', $slug) -> first();
+        }
+        // 4) assign to class
+        $new_category -> slug = $slug;
+
+        $new_category -> save();
+
+        return redirect() -> route('admin.categories.index');
     }
 
     /**
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -6,27 +6,42 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>All categories</h1>
+                <h1>Post's categories</h1>
             </div>
         </div>
 
         <div class="row">
             <div class="col-12">
-                <a href="#" class="btn btn-primary my-2">
-                    Crea una nuova categoria
-                </a>
+                <h2>Crea una nuova categoria</h2>
+
+                <form method="POST" action="{{ route('admin.categories.store') }}">
+                    {{-- token --}}
+                    @csrf
+
+                    {{-- titolo --}}
+                    <div class="form-group">
+                        <label>Nome</label>
+                        <input type="text" class="form-control" name="name" placeholder="Inserisci il titolo">
+                    </div>
+
+                    {{-- sumbit --}}
+                    <button type="submit" class="btn btn-primary my-2">Crea nuova categoria</button>
+                </form>
             </div>
         </div>
 
         {{-- table: all cat --}}
         <div class="row">
             <div class="col-12">
+                <h2>Tutti le categorie</h2>
+
                 <table class="table">
                     <thead class="thead-dark">
                         <tr>
                             <th scope="col">id</th>
                             <th scope="col">Nome</th>
                             <th scope="col">Slug</th>
+                            <th scope="col">Creata</th>
                             <th scope="col">Edita</th>
                         </tr>
                     </thead>
@@ -36,10 +51,8 @@
                                 <th scope="row">{{ $category -> id }}</th>
                                 <td>{{ $category -> name }}</td>
                                 <td>{{ $category -> slug }}</td>
+                                <td>{{ $category -> created_at }}</td>
                                 <td class="w-25">
-                                    <a href="#" class="btn btn-outline-info">
-                                        Vedi
-                                    </a>
                                     <a href="#" class="btn btn-outline-dark">
                                         Modifica
                                     </a>
