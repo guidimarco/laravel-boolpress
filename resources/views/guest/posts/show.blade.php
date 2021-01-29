990165dc27178b00a65a10422bbf07288845437d
Marco Guidi <mark.88g@gmail.com>
2021-01-29 15:35:01 +0100

    + Tag model + Tag migration

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -7,8 +7,8 @@
         <div class="row">
             <div class="col-12">
                 <h1>{{ $post -> title }}</h1>
-                <span>Categoria: {{ $post -> category ? $post -> category -> name : '-' }}</span>
                 <p>{{ $post -> text }}</p>
+                <span>Categoria: {{ $post -> category ? $post -> category -> name : '-' }}</span><br/>
                 <span>Post del: {{ date_format($post -> created_at, 'd/m/Y') }}</span>
             </div>
             <div class="col-12">
