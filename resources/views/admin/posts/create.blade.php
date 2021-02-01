9b24f8ee1fc33b05ad1a02e19f3ac3965e2f6f10
Marco Guidi <mark.88g@gmail.com>
2021-02-01 15:43:27 +0100

    + singolo msg errore

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -25,18 +25,27 @@
                     {{-- titolo --}}
                     <div class="form-group">
                         <label>Titolo</label>
+                        @error ('title')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <input type="text" class="form-control" name="title" placeholder="Inserisci il titolo" required>
                     </div>
 
                     {{-- contenuto --}}
                     <div class="form-group">
                         <label>Contenuto</label>
+                        @error ('text')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <textarea class="form-control" name="text" placeholder="Inserisci l'articolo" row="10" style="height: 200px;" required></textarea>
                     </div>
 
                     {{-- category --}}
                     <div class="form-group">
                         <label>Categoria</label>
+                        @error ('category_id')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <select class="custom-select" name="category_id">
                             <option value="">Seleziona una categoria</option>
                             @foreach ($categories as $category)
@@ -47,6 +56,9 @@
 
                     {{-- tags --}}
                     <span>Tags:</span>
+                    @error ('tags')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                     @foreach ($tags as $tag)
                         <div class="form-check">
                             <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag -> id }}">
