12a181788dfc3bf3665c1a855a428e5e4db0a2ca
Marco Guidi <mark.88g@gmail.com>
2021-02-01 17:25:06 +0100

    + stampati dati utente

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,6 +8,12 @@
             <div class="col-12">
                 <h1>Dashboard</h1>
                 <p>Ciao {{ $this_user -> name }}!</p>
+                <p>I tuoi dati:</p>
+                <ul>
+                    <li>Nome: {{ Auth::user() -> name }}</li>
+                    <li>Mail: {{ Auth::user() -> email }}</li>
+                    <li>Token: {{ Auth::user() -> api_token }}</li>
+                </ul>
             </div>
         </div>
     </div>
