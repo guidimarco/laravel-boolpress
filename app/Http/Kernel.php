629146c753470794b255b318748322e370907517
Marco Guidi <mark.88g@gmail.com>
2021-02-01 18:08:00 +0100

    % correzione percorso middleware nel KERNEL

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'api_token' => \Illuminate\Auth\Middleware\CheckToken::class,
+        'api_token' => \App\Http\Middleware\CheckToken::class,
     ];
 }
