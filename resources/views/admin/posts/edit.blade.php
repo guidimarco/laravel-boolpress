fea07a3aec95ddf7ff22c274a00637e60d233e8d
Marco Guidi <mark.88g@gmail.com>
2021-02-01 16:27:32 +0100

    + msg di errore in update

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -12,9 +12,23 @@
                     @csrf
                     @method('PUT')
 
+                    {{-- Errori validazione --}}
+                    @if ($errors -> any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors -> all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     {{-- titolo --}}
                     <div class="form-group">
                         <label>Titolo</label>
+                        @error ('title')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <input type="text" class="form-control" name="title" placeholder="Inserisci il titolo"
                         value="{{ old('title', $post -> title) }} required">
                     </div>
@@ -22,6 +36,9 @@
                     {{-- contenuto --}}
                     <div class="form-group">
                         <label>Contenuto</label>
+                        @error ('text')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <textarea class="form-control" name="text" placeholder="Inserisci l'articolo" row="10" style="height: 200px;" required>{{
                             old('text', $post -> text)
                         }}</textarea>
@@ -30,6 +47,9 @@
                     {{-- category --}}
                     <div class="form-group">
                         <label>Categoria</label>
+                        @error ('category_id')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
                         <select class="custom-select" name="category_id">
                             <option value="">Seleziona una categoria</option>
                             @foreach ($categories as $category)
@@ -43,6 +63,9 @@
 
                     {{-- tag --}}
                     <span>Tags:</span>
+                    @error ('tags')
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
                     @foreach ($tags as $tag)
                         <div class="form-check">
                             <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag -> id }}"
